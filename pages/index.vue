f356fd6a22a6a0ff91b4ad76d77098428c6baed0
Liran Tal <liran.tal@gmail.com>
2024-08-28 13:00:35 +0300

    feat: add nuxt auth utils support for session management
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-    Hello
+    <div>
+        Hello
+    </div>
+
+    <div>
+        <div v-if="loggedIn">
+            <p>Welcome back, {{ user.name }}!</p>
+            <button @click="clear">Log out</button>
+        </div>
+        <div v-else>
+            <p>You are not logged in.</p>
+            <nuxt-link to="/login">Log in</nuxt-link>
+        </div>
+    </div>
+    
+
 </template>
+
+<script setup>
+const { loggedIn, user, session, fetch, clear } = useUserSession()
+</script>
