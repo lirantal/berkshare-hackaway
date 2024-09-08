a231895b56d3101d4c6edb790aa7681c59450806
Liran Tal <liran.tal@gmail.com>
2024-09-08 14:44:44 +0300

    feat: add support for frontend form to create a memo
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- pages/memos.vue
+++ pages/memos.vue
@@ -8,40 +8,49 @@
         <CardContent>
         <form>
             <div class="grid items-center w-full gap-4">
-            <div class="flex flex-col space-y-1.5">
-                <Label for="name">Memo</Label>
-                <Textarea
-                    id="memo"
-                    default-value="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc."
-                    class="min-h-24"
-                />
-            </div>
-            <div class="flex flex-col space-y-1.5">
-                <Label for="urgency">Urgency</Label>
-                <Select>
-                <SelectTrigger id="urgency">
-                    <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                    <SelectItem value="low">
-                    Low
-                    </SelectItem>
-                    
-
-                </SelectContent>
-                </Select>
-            </div>
+              <div class="flex flex-col space-y-1.5">
+                  <Label for="text">Memo</Label>
+                  <Textarea
+                      v-model="text"
+                      id="text"
+                      placeholder="What can we help you with today?"
+                      class="min-h-24"
+                  />
+              </div>
+              <div class="flex flex-col space-y-1.5">
+                  <Label for="Phone">Callback phone number</Label>
+                  <Input v-model="phone" id="phone" />
+              </div>
+              <div class="flex flex-col space-y-1.5">
+                  <Label for="urgency">Urgency</Label>
+                  <Select v-model="urgency">
+                  <SelectTrigger id="urgency">
+                      <SelectValue placeholder="Select" />
+                  </SelectTrigger>
+                  <SelectContent position="popper">
+                      <SelectItem value="low">
+                      Low
+                      </SelectItem>
+                      <SelectItem value="medium">
+                      Medium
+                      </SelectItem>
+                      <SelectItem value="high">
+                      High
+                      </SelectItem>
+                  </SelectContent>
+                  </Select>
+              </div>
             </div>
         </form>
         </CardContent>
-        <CardFooter class="flex justify-between px-6 pb-6">
-        <Button>Send</Button>
+        <CardFooter class="flex justify-end px-6 pb-6">
+        <Button @click="sendMemo" >Send</Button>
         </CardFooter>
     </Card>
 
     <hr class="m-10"/>
 
-    <Card class="max-w-4xl mt-4" v-for="memo in data.memos" :key="memo.id">
+    <Card class="max-w-4xl mt-4" v-for="memo in data?.memos" :key="memo.id">
     <CardContent>
         <div class="flex items-center gap-4 mt-4">
               <Avatar class="hidden h-9 w-9 sm:flex">
@@ -81,6 +90,34 @@ import { Label } from '@/components/ui/label'
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
 
-const { data, status } = await useFetch('/api/memos')
+import { useFetch } from '@vueuse/core'
+
+const { data, refresh } = await useFetch('/api/memos')
+
+const text = defineModel('text');
+const phone = defineModel('phone');
+const urgency = defineModel('urgency');
+
+const sendMemo = async () => {
+  
+  const response = await $fetch('/api/memos', {
+    method: 'POST',
+    body: {
+      text: text.value,
+      meta: JSON.stringify({
+        phone: phone.value,
+        urgency: urgency.value
+      })
+    }
+  })
+
+  if (response?.success === true) {
+    text.value = ''
+    phone.value = ''
+    urgency.value = ''
+    refresh()
+  }
+
+}
 
 </script>
