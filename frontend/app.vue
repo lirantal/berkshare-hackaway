d7d24599ee043c39b4e70cc20943f989dc47a72a
Liran Tal <liran.tal@gmail.com>
2024-09-09 15:33:00 +0300

    refactor: relocate login to the app level
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,30 @@
 <template>
-    <NuxtLayout>
-        <NuxtPage />
-    </NuxtLayout>
+    <div>
+        <div v-if="loading" class="h-screen w-screen flex justify-center items-center">
+            Loading ...
+        </div>
+        <NuxtLayout v-else>
+            <NuxtPage />
+        </NuxtLayout>
+    </div>
 </template>
+
+<script setup>
+let loading = ref(true)
+let loggedIn = ref(false)
+let user = ref({})
+
+onMounted(async () => {
+    try {
+        const userDetails = await $fetch('/api/user')
+        user.value = userDetails
+        loggedIn.value = !!userDetails.user_id
+        loading.value = false
+    } catch (error) {
+        console.error(error)
+    } finally {
+        loading.value = false
+    }
+})
+
+</script>
