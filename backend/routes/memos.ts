cad1608ebcef6437c28ee83b95c7eaeca6c65ddc
Liran Tal <liran.tal@gmail.com>
2024-09-15 11:44:50 +0200

    fix: allow admins to read everyone's memos
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- backend/routes/memos.ts
+++ backend/routes/memos.ts
@@ -3,59 +3,82 @@ import { db } from "../lib/db.js";
 
 export const memosRouter = express.Router();
 
-memosRouter.get('/memos', (req, res) => {
-    if (!res.locals.user) {
-        return res.status(401).send('Unauthorized');
-    }
+memosRouter.get("/memos", (req, res) => {
+  if (!res.locals.user) {
+    return res.status(401).send("Unauthorized");
+  }
 
-    const memos = db.prepare(`SELECT memos.id, memos.text, memos.meta, memos.date, user_profile.full_name, user_profile.user_id FROM memos JOIN 
-        user_profile ON memos.user_id = user_profile.user_id WHERE memos.user_id = ?`).all(res.locals.user.id);
+  let memosList = [];
+  if (res.locals.user.isAdmin) {
+    // get memos for all users:
+    memosList = db
+      .prepare(
+        `SELECT memos.id, memos.text, memos.meta, memos.date, user_profile.full_name, user_profile.user_id
+         FROM memos
+         JOIN user_profile ON memos.user_id = user_profile.user_id`
+      )
+      .all();
+  } else {
+    memosList = db
+      .prepare(
+        `SELECT memos.id, memos.text, memos.meta, memos.date, user_profile.full_name, user_profile.user_id FROM memos JOIN 
+            user_profile ON memos.user_id = user_profile.user_id WHERE memos.user_id = ?`
+      )
+      .all(res.locals.user.id);
+  }
 
-    return res.status(200).json({
-        success: true,
-        memos
-    });
+  return res.status(200).json({
+    success: true,
+    memos: memosList,
+  });
 });
 
-memosRouter.post('/memos', (req, res) => {
-    if (!res.locals.user) {
-        return res.status(401).send('Unauthorized');
-    }
+memosRouter.post("/memos", (req, res) => {
+  if (!res.locals.user) {
+    return res.status(401).send("Unauthorized");
+  }
 
-    const text = req.body.text;
-    const meta = req.body.meta || '{}';
+  const text = req.body.text;
+  const meta = req.body.meta || "{}";
 
-    if (!text) {
-        return res.status(400).json({
-            success: false,
-            error: 'Text is required'
-        });
-    }
-
-    const baseContactInformation = db.prepare(`SELECT user_profile.email, user_profile.address, user_profile.phone_number FROM user_profile WHERE user_profile.user_id = ?`).get(res.locals.user.id);
-    const contactInMemo = JSON.parse(meta);
-    const userExtendedMeta = recursiveJSONMerge(baseContactInformation, contactInMemo);
-
-    const date = new Date().toISOString();
-    db.prepare('INSERT INTO memos (user_id, text, meta, date) VALUES (?, ?, ?, ?)').run(res.locals.user.id, text, JSON.stringify(userExtendedMeta), date);
-
-    return res.status(200).json({
-        success: true
+  if (!text) {
+    return res.status(400).json({
+      success: false,
+      error: "Text is required",
     });
+  }
+
+  const baseContactInformation = db
+    .prepare(
+      `SELECT user_profile.email, user_profile.address, user_profile.phone_number FROM user_profile WHERE user_profile.user_id = ?`
+    )
+    .get(res.locals.user.id);
+  const contactInMemo = JSON.parse(meta);
+  const userExtendedMeta = recursiveJSONMerge(
+    baseContactInformation,
+    contactInMemo
+  );
+
+  const date = new Date().toISOString();
+  db.prepare(
+    "INSERT INTO memos (user_id, text, meta, date) VALUES (?, ?, ?, ?)"
+  ).run(res.locals.user.id, text, JSON.stringify(userExtendedMeta), date);
+
+  return res.status(200).json({
+    success: true,
+  });
 });
 
 function recursiveJSONMerge(target, source) {
-    for (const key in source) {
-        if (source[key] instanceof Object) {
-            target[key] = recursiveJSONMerge(target[key] || {}, source[key]);
-        } else {
-            target[key] = source[key];
-        }
+  for (const key in source) {
+    if (source[key] instanceof Object) {
+      target[key] = recursiveJSONMerge(target[key] || {}, source[key]);
+    } else {
+      target[key] = source[key];
     }
-    return target;
+  }
+  return target;
 }
-
-
 
 // ## ok request
 // curl 'http://localhost:3000/api/memos' \
@@ -74,7 +97,6 @@ function recursiveJSONMerge(target, source) {
 //   -H 'sec-ch-ua-mobile: ?0' \
 //   -H 'sec-ch-ua-platform: "macOS"' \
 //   --data-raw '{"text":"ok ok ok ","meta":"{\"phone\":\"1234\",\"urgency\":\"high\"}"}'
-
 
 // ## exploit request
 // curl 'http://localhost:3000/api/memos' \
