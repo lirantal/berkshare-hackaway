05e31e39757b9873015302210484e24e9c0f3ffd
Liran Tal <liran.tal@gmail.com>
2024-09-16 16:36:37 +0200

    feat: add banking app name onto the page nav
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
 <div class="flex min-h-screen max-w-7xl flex-col justify-center mx-auto">
 <header class="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+  <div>
+    <p class="whitespace-nowrap font-bold">
+      Berkshare Hackaway
+    </p>
+  </div>
       <nav class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <a
           href="#"
